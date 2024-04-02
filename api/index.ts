8141b4237a49eaec1cf7331526005ed39724c57d
achmedzhanov <achmedzhanov@gmail.com>
2024-04-02 23:37:36 +0400

    -chore: try proxy

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,33 @@
-const express = require("express");
+const express = require('express');
+const proxy = require('express-http-proxy');
 const app = express();
+const targetUrl = 'https://api.openai.com';
+//const openaiKey = process.env.OPENAI_KEY
+const port = 3000;
+//const baseUrl = getExternalUrl(process.env.SPACE_ID);
 
-app.get("/", (req, res) => res.send("Express on Vercel nested"));
+app.use('/', proxy(targetUrl, {
+  proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+    // Modify the request headers if necessary
+    // proxyReqOpts.headers['Authorization'] = 'Bearer '+openaiKey;
+    return proxyReqOpts;
+  },
+}));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.get("/", (req, res) => {
+  //res.send(`This is your OpenAI Reverse Proxy URL: ${baseUrl}`);
+  res.send('This is your OpenAI Reverse Proxy');
+});
 
-module.exports = app;
+/*function getExternalUrl(spaceId) {
+  try {
+    const [username, spacename] = spaceId.split("/");
+    return `https://${username}-${spacename.replace(/_/g, "-")}.hf.space/api/v1`;
+  } catch (e) {
+    return "";
+  }
+}*/
+
+app.listen(port, () => {
+//  console.log(`Reverse proxy server running on ${baseUrl}`);
+  console.log('Reverse proxy server running');})
