abb58b95995e4020a279e97369fdc0c8b510b1e8
achmedzhanov <achmedzhanov@gmail.com>
2024-04-18 12:08:35 +0400

    -chore: logging, systemd config

--- server.js
+++ server.js
@@ -1,42 +1,49 @@
-const express = require("express");
+
+const express = require('express');
+const proxy = require('express-http-proxy');
 const app = express();
+const targetUrl = 'https://api.openai.com';
+const port = 3000;
 
-app.get("/", (req, res) => res.send("Express on Vercel - root!"));
+// app.use((req, res, next) => {
+//     const start = Date.now();
+//     res.on('finish', () => {
+//       const duration = Date.now() - start;
+//       console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl} - ${res.statusCode} (${duration}ms)`);
+//     });
+//     next();
+//   });
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.use((req, res, next) => {
+    const start = Date.now();
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    let bodySize = 0;
+  
+    res.on('data', (chunk) => {
+      bodySize += chunk.length;
+    });
+  
+    res.on('finish', () => {
+      const duration = Date.now() - start;
+      console.log(`[${new Date().toISOString()}] ${ip} - ${req.method} ${req.originalUrl} - ${res.statusCode} (${bodySize} bytes, ${duration}ms)`);
+    });
+  
+    next();
+  });
 
-module.exports = app;
+app.use('/api', proxy(targetUrl, {
+  proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+    // Modify the request headers if necessary
+    // proxyReqOpts.headers['Authorization'] = 'Bearer '+openaiKey;
+    return proxyReqOpts;
+  },
+}));
 
-// const express = require('express');
-// const proxy = require('express-http-proxy');
-// const app = express();
-// const targetUrl = 'https://api.openai.com';
-// //const openaiKey = process.env.OPENAI_KEY
-// const port = 3000;
-// //const baseUrl = getExternalUrl(process.env.SPACE_ID);
+app.get("/", (req, res) => {
+  res.send('This is OAIRP');
+});
 
-// app.use('/api', proxy(targetUrl, {
-//   proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
-//     // Modify the request headers if necessary
-//     // proxyReqOpts.headers['Authorization'] = 'Bearer '+openaiKey;
-//     return proxyReqOpts;
-//   },
-// }));
 
-// app.get("/", (req, res) => {
-//   //res.send(`This is your OpenAI Reverse Proxy URL: ${baseUrl}`);
-//   res.send('This is your OpenAI Reverse Proxy');
-// });
-
-// /*function getExternalUrl(spaceId) {
-//   try {
-//     const [username, spacename] = spaceId.split("/");
-//     return `https://${username}-${spacename.replace(/_/g, "-")}.hf.space/api/v1`;
-//   } catch (e) {
-//     return "";
-//   }
-// }*/
-
-// app.listen(port, () => {
-// //  console.log(`Reverse proxy server running on ${baseUrl}`);
-//   console.log('Reverse proxy server running');})
+app.listen(port, () => {
+//  console.log(`Reverse proxy server running on ${baseUrl}`);
+  console.log('OpenAI Reverse proxy server running on port ' + port);})
